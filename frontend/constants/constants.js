07760cdf958503427c9a20df1c96c589c5fa1470
ryan <ryanfoong8983@gmail.com>
2019-04-18 14:40:46 -0700

    Add conditional for cable env vars

--- frontend/constants/constants.js
+++ frontend/constants/constants.js
@@ -1,5 +1,18 @@
-export const API_ROOT = 'http://localhost:3000';
-export const API_WS_ROOT = 'ws://localhost:3000/cable';
+let API_ROOT;
+let API_WS_ROOT;
+
+if (process.env.NODE_ENV !== 'production') {
+    API_ROOT = 'http://localhost:3000';
+    API_WS_ROOT = 'ws://localhost:3000/cable';
+} else {
+    API_ROOT = 'https://disclone-rf.herokuapp.com';
+    API_WS_ROOT = 'ws://disclone-rf.herokuapp.com/cable';
+}
+// export const API_ROOT = 'http://localhost:3000';
+// export const API_WS_ROOT = 'ws://localhost:3000/cable';
+
+export { API_ROOT, API_WS_ROOT };
+
 export const HEADERS = {
     'Content-Type': 'application/json',
     Accept: 'application/json'
